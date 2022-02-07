33ac78566add58a24f4871e0d4546f747f6bea0c
yurii.bondar <yurii.bondar@ria.com>
2022-02-08 00:13:55 +0200

    Added logs to middlewares

--- app/app.js
+++ app/app.js
@@ -8,19 +8,29 @@ const app = new Koa();
 app.use(publicRoutes());
 app.use(privateRoutes());
 
-app.use(async (ctx, next) => {
-    await next();
+app.use((ctx, next) => {
+    console.info('start M1, upstream to M2');
+    next();
+    console.info('continue to M1, downstream finish')
     const resTime = ctx.response.get('X-Response-Time');
     if(ctx.url !== '/favicon.ico')
-    console.log(`${ctx.method} ${ctx.url} -> ${resTime}`);
+    console.info(`METHOD -> "${ctx.method}" URL -> "${ctx.url}" RESPONSE_TIME -> "${resTime}"`);
 });
 
-app.use(async (ctx, next) => {
+app.use((ctx, next) => {
+    console.info('start M2, upstream to M3')
     const startPoint = Date.now();
-    await next();
+    next();
+    console.info('continue to M2, downstream to M1')
     const duration = Date.now() - startPoint;
     ctx.set('X-Response-Time', `${duration} ms.`)
 });
+
+app.use((ctx) => {
+    console.info('start M3, downstream to M2')
+    const osName = ctx.request.headers['x-mobile-os-name'] || 'unknown';
+    console.info(`request from ${osName.toUpperCase()}`);
+})
 
 const {port} = config.server;
 app.listen(port, () =>
